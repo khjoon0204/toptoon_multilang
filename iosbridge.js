ad9768a6d61a9dbcddb0099a1c72dbc7b2756b61
khjoon0204 <khjoon@toptoon.com>
2017-05-23 16:47:30 +0900

    Add files via upload

--- iosbridge.js
+++ iosbridge.js
@@ -47,6 +47,38 @@ var iosbridge = (function(namespace, $, undefined){
     	
     	/* Initialize your app here */
 
+
+		var uniqueId = 1
+		function log(message, data) {
+			var log = document.getElementById('log')
+			var el = document.createElement('div')
+			el.className = 'logLine'
+			el.innerHTML = uniqueId++ + '. ' + message + ':<br/>' + JSON.stringify(data)
+			if (log.children.length) { log.insertBefore(el, log.children[0]) }
+			else { log.appendChild(el) }
+		}
+
+		bridge.registerHandler('testJavascriptHandler', function(data, responseCallback) {
+			log('ObjC called testJavascriptHandler with', data)
+			var responseData = { 'Javascript Says':'Right back atcha!' }
+			log('JS responding with', responseData)
+			responseCallback(responseData)
+		})
+
+		document.body.appendChild(document.createElement('br'))
+
+		var callbackButton = document.getElementById('buttons').appendChild(document.createElement('button'))
+		callbackButton.innerHTML = 'Fire testObjcCallback'
+		callbackButton.onclick = function(e) {
+			e.preventDefault()
+			log('JS calling handler "testObjcCallback"')
+			bridge.callHandler('testObjcCallback', {'foo': 'bar'}, function(response) {
+				log('JS got response', response)
+			})
+		}
+    	
+    	
+    	/*
     	bridge.registerHandler(registerHandlerName, function(data, responseCallback) {
 			log('ObjC called testJavascriptHandler with', data)
 			var responseData = { 'Javascript Says':'Right back atcha!' }
@@ -56,6 +88,7 @@ var iosbridge = (function(namespace, $, undefined){
     	bridge.callHandler(callHandlerName, {'foo': 'bar'}, function(response) {
 			log('JS got response', response)
 		})
+		*/
     })
     
     namespace.call = function(data){ //외부 노출 함수 public
